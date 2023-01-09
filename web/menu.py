cee1acf784660de799b82525e14a405b4a2a0473
luimungar2 <luismunizgarcia@hotmail.es>
2023-01-09 11:08:00 -0500

    changed abs path, enabled with scan class

--- web/menu.py
+++ web/menu.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template      
+import sys
+from flask import Flask, render_template, request
+from pathlib import Path
+abspath = str(Path.cwd())
+abspath = abspath.replace('/web','')
+sys.path.insert(0, abspath+'/scanner/') 
+print (abspath+'/scanner/')
+from nmap import Scanner
 
 app = Flask(__name__)
 
@@ -12,11 +19,24 @@ def luis():
 
 @app.route('/show_data',  methods=("POST", "GET"))
 def showData():
-    # Convert pandas dataframe to html table flask
-    fichero = open('/home/kali/Desktop/CybersecAssistant/scanner/resultado_escaneo.txt','r')
-    contenido  = fichero.read()
-    print(contenido)
-    return render_template('prueba.html', data=contenido)
+    if request.method == "POST":
+        # getting input with name = fname in HTML form
+        ip = request.form.get("IP")
+        print(ip)
+        # getting input with name = lname in HTML form
+        filename = request.form.get("FILENAME")
+        print(filename)
+        scanner = Scanner(ip, filename)
+        print("escaner creado")
+        scanner.scan_services()
+        fichero = open(filename,'r')
+        contenido  = fichero.read()
+        print(contenido)
+        return render_template('prueba.html', data=contenido)
+    else:
+        return render_template('404.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host = '192.168.226.131', port = 5000)
