f0618fbe1c39b64da4edd562853dbac48bd0551d
luimungar2 <luismunizgarcia@hotmail.es>
2023-01-09 06:21:21 -0500

    added a tab to display the scan results. tbd improve this feature and parse the nmap info

--- web/menu.py
+++ web/menu.py
@@ -9,6 +9,14 @@ def home():
 @app.route("/luis")
 def luis():
     return "Hello, Luis"
-    
+
+@app.route('/show_data',  methods=("POST", "GET"))
+def showData():
+    # Convert pandas dataframe to html table flask
+    fichero = open('/home/kali/Desktop/CybersecAssistant/scanner/resultado_escaneo.txt','r')
+    contenido  = fichero.read()
+    print(contenido)
+    return render_template('prueba.html', data=contenido)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host = '192.168.226.131', port = 5000)
