fd471e3ddba5eaa526635b844f12cc66775712fe
luimungar2 <luismunizgarcia@hotmail.es>
2023-01-10 04:49:01 -0500

    added discover eature to webpage

--- web/menu.py
+++ web/menu.py
@@ -6,6 +6,7 @@ abspath = abspath.replace('/web','')
 sys.path.insert(0, abspath+'/scanner/') 
 print (abspath+'/scanner/')
 from nmap import Scanner
+from netdiscover import Discover
 
 app = Flask(__name__)
 
@@ -17,8 +18,8 @@ def home():
 def luis():
     return "Hello, Luis"
 
-@app.route('/show_data',  methods=("POST", "GET"))
-def showData():
+@app.route('/scan',  methods=("POST", "GET"))
+def show_scan():
     if request.method == "POST":
         # getting input with name = fname in HTML form
         ip = request.form.get("IP")
@@ -32,11 +33,30 @@ def showData():
         fichero = open(filename,'r')
         contenido  = fichero.read()
         print(contenido)
-        return render_template('prueba.html', data=contenido)
+        return render_template('nmap.html', data=contenido)
     else:
         return render_template('404.html')
 
-
+@app.route('/discover',  methods=("POST", "GET"))
+def show_discover():
+    if request.method == "POST":
+        # getting input with ip = IP and mask = MASK in HTML form
+        ip = request.form.get("IP")
+        print(ip)
+        mask = request.form.get("MASK")
+        print(mask)
+        # getting input with filename = FILENAME in HTML form
+        filename = request.form.get("FILENAME")
+        print(filename)
+        discover = Discover(ip, mask, filename)
+        print("sonda creada")
+        discover.discover()
+        fichero = open(filename,'r')
+        contenido  = fichero.read()
+        print(contenido)
+        return render_template('discover.html', data=contenido)
+    else:
+        return render_template('404.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host = '192.168.226.131', port = 5000)
